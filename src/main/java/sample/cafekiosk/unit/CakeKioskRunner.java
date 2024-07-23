d776911e1a1d91157960f79bd706d833f73bf9a7
DongYeop <s01054830455@gmail.com>
2024-07-23 19:27:20 +0900

    feat: 테스트 기본 환경 세팅

---
+++ src/main/java/sample/cafekiosk/unit/CakeKioskRunner.java
@@ -0,0 +1,22 @@
+package sample.cafekiosk.unit;
+
+import sample.cafekiosk.unit.beverage.Americano;
+import sample.cafekiosk.unit.beverage.Latte;
+
+public class CakeKioskRunner {
+
+    public static void main(String[] args) {
+        CakeKiosk cakeKiosk = new CakeKiosk();
+
+        cakeKiosk.add(new Americano());
+        System.out.println(">>>> 아메리카노 추가");
+
+        cakeKiosk.add(new Latte());
+        System.out.println(">>>> 라떼 추가");
+
+        int totalPrice = cakeKiosk.calculateTotalPrice();
+
+        System.out.println("총 주문 가격 : " + totalPrice);
+
+    }
+}
